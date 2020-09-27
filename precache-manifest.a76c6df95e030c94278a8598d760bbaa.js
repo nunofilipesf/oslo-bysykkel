162f05b788589d2a99e91ab88f406018d742c6e9
Nuno Ferreira <nuno.ferreira@intellilift.no>
2020-09-27 10:40:43 +0200

    Updates

---
+++ precache-manifest.a76c6df95e030c94278a8598d760bbaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7c162afc134a9865b5e22a2047cca2",
+    "url": "/oslo-bysykkel/index.html"
+  },
+  {
+    "revision": "ec37d4b281217e0569ee",
+    "url": "/oslo-bysykkel/static/css/main.e7715761.chunk.css"
+  },
+  {
+    "revision": "4c20fddc7dba26681ccd",
+    "url": "/oslo-bysykkel/static/js/2.810c8ef4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oslo-bysykkel/static/js/2.810c8ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec37d4b281217e0569ee",
+    "url": "/oslo-bysykkel/static/js/main.9cd5de41.chunk.js"
+  },
+  {
+    "revision": "ec8126a9ebf26ead88b2",
+    "url": "/oslo-bysykkel/static/js/runtime-main.3be423b1.js"
+  }
+]);
